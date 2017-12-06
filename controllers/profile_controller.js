d4bd7ca55777a08ef30d6a2abbb2a2b8500745ea
Nicole Chow <nicolechow@Nicoles-MacBook-Pro.local>
2017-12-05 20:06:20 -0800

    modified folder structure for app and structure for login

---
+++ controllers/profile_controller.js
@@ -0,0 +1,33 @@
+var db  = require('../models');
+var express = require('express');
+var router  = express.Router();
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+// only show profile if signed in
+router.get('/', isAuthenticated, function(req, res) {
+
+	db.User.findAll({
+    where: {
+    	UserId: req.user.id
+    }
+  }).then(function(dbUser) {
+  	console.log(dbUser);
+    res.render('profiles/profiles', {
+  		layout: 'main-profiles',
+  		// trip: dbTrip
+  	});
+  });
+
+});
+
+router.post('/new', isAuthenticated, function(req, res) {
+
+	// Add id from User onto req.body
+	req.body.UserId = req.user.id;
+
+  db.User.create(req.body).then(function(dbPost) {
+    res.json(dbPost);
+  });
+});
+
+module.exports = router;
